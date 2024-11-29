63cb8824a6da2bce331af610740660904baec4cf
aladamm78 <aladamm78@gmail.com>
2024-11-29 13:11:58 -0500

    step 2

---
+++ src/api.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+
+/** API Class.
+ *
+ * Static class tying together methods used to get/send to to the API.
+ * There shouldn't be any frontend-specific stuff here, and there shouldn't
+ * be any API-aware stuff elsewhere in the frontend.
+ *
+ */
+
+class JoblyApi {
+  // the token for interacting with the API will be stored here.
+  static token;
+
+  static async request(endpoint, data = {}, method = "get") {
+    console.debug("API Call:", endpoint, data, method);
+
+    // there are multiple ways to pass an authorization token, this is how you pass it in the header.
+    // this has been provided to show you another way to pass the token. You are only expected to read this code for this project.
+    const url = `${BASE_URL}/${endpoint}`;
+    const headers = { Authorization: `Bearer ${JoblyApi.token}` };
+    const params = method === "get" ? data : {};
+
+    try {
+      return (await axios({ url, method, data, params, headers })).data;
+    } catch (err) {
+      console.error("API Error:", err.response);
+      let message = err.response.data.error.message;
+      throw Array.isArray(message) ? message : [message];
+    }
+  }
+
+  // Individual API routes
+
+  /** Get details on a company by handle. */
+  static async getCompany(handle) {
+    let res = await this.request(`companies/${handle}`);
+    return res.company;
+  }
+
+  // obviously, you'll add a lot here ...
+}
+
+// Set token for current user
+JoblyApi.token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsYWRhbW03OCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTczMjQwMTA2NX0.6mJIqZ0gpcRd1TbJJvWJmx78wNqbkB1T2r_C9uLqfrc";
+
+export default JoblyApi;
